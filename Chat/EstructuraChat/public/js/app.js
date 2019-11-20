e8f651b7504607e1fdce013e8ad5d352fd9ec7d4
Cesar Franco <>
2019-11-19 23:14:21 -0500

    Se agrega nueva versión del chat con ajustes genera les a los sockets

--- Chat/EstructuraChat/public/js/app.js
+++ Chat/EstructuraChat/public/js/app.js
@@ -51,8 +51,8 @@ function slideContactos(direction){
 
   }
 }
-
 var desplegado = false;
+
 $(function(){
   //----VARIABLES Y FUNCIONES DEL DISEÑO ADAPTATIVO -----//
   $(".container").css("height",$(window).height());
@@ -70,3 +70,168 @@ $(function(){
     })
   }
 })
+
+(function (document, window, undefined, $, io) {
+  (function () {
+      return Chat = {
+          //Todo el codifo va aqui
+          apiUrl: '/chat',
+          $userDataModel: $('#modalCaptura'),
+          $btnMessage: $('#btnMessage'),
+          $messageText: $('#messageText'),
+          userName: '',
+          socket: io(),
+
+          //Constructor
+          Init: function () {
+              var self = this; //Para mantener y obtener el contexto de js
+              this.fetchUserInfo(function (user) {
+                  self.renderUser(user);
+              })
+              this.watchMessages()
+              self.socket.on('userJoin', function(user){
+                  self.renderUser(user)
+              })
+              self.socket.on('message', function(message){
+                  self.renderUser(message)
+              })
+          },
+
+          fetchUserInfo: function (callBack) {
+              var self = this;
+              this.$userDataModel.openModal();
+              var $GuardarInfo = $('.guardaInfo')
+              $GuardarInfo.on('click', function () {
+                  var nombre = $('.nombreUsuario').val()
+                  var user = [{
+                      nombre: nombre,
+                      img: 'p2.png'
+                  }]
+                  self.socket.emit('userJoin', user[0])
+                  callBack(user)
+
+                  self.joinUser(user[0])
+
+
+                  self.userName = nombre
+                  self.$userDataModel.closeModal()
+              });
+              self.getInitialUsers()
+          },
+
+          getInitialUsers: function () {
+              var self = this;
+              var endpoint = self.apiUrl + '/users'
+              self.ajaxRequest(endpoint, 'GET', {})
+                  .done(function (data) {
+                      var users = data.current
+                      self.renderUser(users)
+                  }).fail(function (error) {
+                      console.log(error);
+                  })
+          },
+
+          ajaxRequest: function (url, type, data) {
+              return $.ajax({
+                  url: url,
+                  type: type,
+                  data: data
+              })
+          },
+
+          joinUser: function (user) {
+              var self = this;
+              var endpoint = self.apiUrl + '/users'
+              var userobj = {
+                  user: user
+              }
+              self.ajaxRequest(endpoint, 'POST', userobj)
+                  .done(function (response) {
+                      console.log(response)
+                  }).fail(function (error) {
+                      alert(error);
+                  })
+          },
+
+          renderUser: function (users) {
+              var self = this;
+              var userList = $('.users-list')
+              var userTemplate = '<li class="collection-item avatar">' +
+                  '<img src="image/:image:" class="circle">' +
+                  '<span class="title">:nombre:</span>' +
+                  '<p><img src="image/online.png"/>En linea </p>' +
+                  '</li>'
+              users.map(function (user) {
+                  var newUsers = userTemplate.replace(':image:', 'p2.jpg')
+                      .replace(':nombre:', users.nombre)
+              })
+          },
+
+          watchMessages: function () {
+              var self = this
+              self.$messageText.on('keypress', function (e) {
+                  if (e.which == 13) {
+                      if ($(this).val().tirm() != '') {
+                          var message = {
+                              sender: self.userName,
+                              text: $(this).val()
+                          }
+                          self.renderMessage(message)
+                          self.socket.emit('message', message)
+                          $(this).val('')
+                      } else {
+                          e.preventDefault()
+                      }
+                  }
+              })
+              self.$btnMessage.on('click', function () {
+                  if (self.$btnMessage.val() != '') {
+                      var message = {
+                          sender: self.userName,
+                          text: $(this).val()
+                      }
+                      self.renderMessage(message)
+                      self.socket.emit('message', message)
+                      self.$messageText.val('')
+                  }
+              })
+          },
+
+          renderMessage: function (message) {
+              var self = this
+              var tipoMensaje = message.sender == self.userName ? 'recibidos' : 'enviado'
+              var messageList = $('.historial-chat')
+              var messageTemplate = '<div class=":tipoMensaje:">' +
+                  '<div class="mensaje">' +
+                  '<div class="imagen">' +
+                  '<img src="image/p2.jpg" alt="Contacto"/>' +
+                  '</div>' +
+                  '<div class="texto">' +
+                  '<span class="nombre">:nombre:</span><br>' +
+                  '<span>:mensaje:</span>' +
+                  '</div>' +
+                  '<div class="hora">' +
+                  '<span class="numHora">:hora:</span>' +
+                  '</div>' +
+                  '</div>' +
+                  '</div>';
+              var currentDate = new Date();
+              var newMenssage = messageTemplate.replace(':tipoMensaje:', tipoMensaje)
+                  .replace(':nombre:', message.sender)
+                  .replace(':mensaje:', message.text)
+                  .replace(':hora:', currentDate.getHours() + ':' + currentDate.getMinutes())
+              messageList.append(newMenssage)
+              $(".srcoller-chat").animate({
+                  scrollTop: $(".scroller-chat").get(0).scrollHeight
+              }, 500)
+
+
+          }
+
+
+      }
+  })();
+
+  Chat.Init();
+  
+})(document, window, undefined, jQuery, io);
--- Chat/EstructuraChat/server/index.js
+++ Chat/EstructuraChat/server/index.js
@@ -1,7 +1,8 @@
 var bodyParser = require('body-parser'),
     http = require('http'),
     express = require('express'),
-    chat = require('./Chat')
+    chat = require('./Chat/index'),
+    socketio = require('socket.io')(app)
 
 
 var port = port = process.env.port || 3000,
@@ -16,3 +17,29 @@ app.use(express.static('public'))
 Server.listen(port,() => {
     console.log("El servidor esta ejecutandose en el puerto: " + port)
 })
+
+socketio.on('connection', function(socket) {
+    console.log('New user connected, socket: ' + socketio.id)
+
+    socket.on('userJoin', function(user) {
+        // Escuchar el evento user join, para agregar para agregar un usuario a los otros sockets
+        socket.user = user
+        socket.broadcast.emit('userJoin', user)
+    })
+
+    socket.on('message', function(message) {
+        //Escuchar evento message, para emitirlo a otros sockets
+        socket.broadcast.emit('message', message)
+    })
+
+    socket.on('disconnect', function() {
+        //Escuchar el evento de dexconexion para eliminar el usario 
+        if(socket.hasOwnProperty('user')) {
+            deleteUser(socket.user, function(err,confirm){
+                if(err) throw err
+            })
+        }
+    })
+
+})
+
